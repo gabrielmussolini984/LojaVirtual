c8f68bee58db1305f59e47271a393219c59cd3f7
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2020-01-11 15:20:53 -0300

    Continuação

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+// DB
+const db = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'shop'
+  }
+});
+db.on('query', query =>{
+  console.log('SQL:',query.sql);
+});
 //App
 const app = express();
 
@@ -11,15 +24,15 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-
+ 
 // Public
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/', (req,res)=>{
-  res.render('layouts/main')
+app.get('/', async(req,res)=>{
+  const categorias = await db('categorias').select('*');
+  console.log(categorias);
+  res.render('layouts/main',{categorias})
 });
-
-
 
 //Port Config and Server
 const port = process.env.PORT || 3000;
