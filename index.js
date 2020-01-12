fdf7432093af778d520e67ecd1e7e908f7e5be93
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2020-01-11 21:07:21 -0300

    Listando Produtos e Configurando Slug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const slug = require('./utils/slug')
 
 // DB
 const db = require('knex')({
@@ -27,12 +28,29 @@ app.set('view engine', 'handlebars');
  
 // Public
 app.use(express.static(path.join(__dirname,'public')));
+//app.use(express.static(__dirname + '/public'));
 
 app.get('/', async(req,res)=>{
   const categorias = await db('categorias').select('*');
-  console.log(categorias);
-  res.render('layouts/main',{categorias})
+  const categoriasComSlug = categorias.map((categoria)=>{
+    const novaCategoria = {...categoria,slug: slug(categoria.categoria)};
+    return novaCategoria
+  })
+  res.render('home',{categorias: categoriasComSlug});
 });
+
+app.get('/categoria/:id', async(req,res)=>{
+  const categoria = await db('categorias').where({id: req.params.id}).select('*');
+  const categorias = await db('categorias').select('*');
+  const produtos = await db('produtos').select('*').where('id', function(){
+    this
+      .select('categorias_produtos.produto_id')
+      .from('categorias_produtos')
+      .whereRaw('categorias_produtos.produto_id = produtos.id')
+      .where('categoria_id', req.params.id);
+  })
+  res.render('categoria',{categorias,estaCategoria: categoria[0],produtos});
+})
 
 //Port Config and Server
 const port = process.env.PORT || 3000;
